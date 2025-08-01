e9d9fed0ab74a7ada6ab99f8c8d632f884be30ce
wangWking <842749351@qq.com>
2025-08-01 19:12:19 +0800

    update

--- main.py
+++ main.py
@@ -3227,7 +3227,8 @@ def resource_path(relative_path):
     if getattr(sys, 'frozen', False):
         base_path = sys._MEIPASS
     else:
-        base_path = os.path.abspath(".")
+        # base_path = os.path.abspath(".")
+        base_path = os.path.dirname(os.path.abspath(__file__))
     return os.path.join(base_path, relative_path)
 
 
